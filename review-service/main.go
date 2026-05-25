e00c5aca37d4618ca0e6c191db31228eb4d868f3
Yernur <erkeerke349@gmail.com>
2026-05-25 10:21:38 +0500

    final

--- review-service/main.go
+++ review-service/main.go
@@ -30,7 +30,7 @@ func main() {
 	r.Use(func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
